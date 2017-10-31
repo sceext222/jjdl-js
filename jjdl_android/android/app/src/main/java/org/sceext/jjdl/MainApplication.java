bfdb0ef03e715581f7a3e449c71df3fbda178798
sceext2 <sceext@foxmail.com>
2017-10-31 21:42:54 +0800

    jjdl_android: add react-native-fs

--- jjdl_android/android/app/src/main/java/org/sceext/jjdl/MainApplication.java
+++ jjdl_android/android/app/src/main/java/org/sceext/jjdl/MainApplication.java
@@ -1,5 +1,8 @@
 package org.sceext.jjdl;
 
+import java.util.Arrays;
+import java.util.List;
+
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
@@ -8,8 +11,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import java.util.Arrays;
-import java.util.List;
+import com.rnfs.RNFSPackage;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -22,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNFSPackage()
       );
     }
 
--- jjdl_android/src/config.coffee
+++ jjdl_android/src/config.coffee
@@ -1,6 +1,6 @@
 # config.coffee, jjdl_android/src/
 
-P_VERSION = 'jjdl_android version 0.1.0-1 test20171031 2009'
+P_VERSION = 'jjdl_android version 0.1.0-1 test20171031 2022'
 
 
 SITE_LIST = [
@@ -9,9 +9,14 @@ SITE_LIST = [
   'myushuwu'
 ]
 
+# root path to store jjdl_android files
+SDCARD_JJDL_ROOT = '/sdcard/jjdl/'
+
 
 module.exports = {
   P_VERSION
 
   SITE_LIST
+
+  SDCARD_JJDL_ROOT
 }
